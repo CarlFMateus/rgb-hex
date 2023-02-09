134b3dc7e896c8e1a0a5dfa121b6d605e2cdb74d
Carl Mateus <carl.italentt@gmail.com>
2023-02-09 16:56:06 -0500

    Improve test failed

---
+++ __tests__/file.test.ts
@@ -0,0 +1,5 @@
+import { pruebaFunc } from '../file'
+
+describe('Esto es una prueba de una función', () => {
+  expect(pruebaFunc(1, 2)).toBeNull()
+})
---
+++ file.ts
@@ -0,0 +1,3 @@
+export function pruebaFunc(a: number, b: number) {
+  return a + b
+}
