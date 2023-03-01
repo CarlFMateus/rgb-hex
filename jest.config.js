21544b2531dfbeaa81eddd00686146fff151c240
Carl Mateus <carl.italentt@gmail.com>
2023-03-01 15:50:57 -0500

    Test files changes pr

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  collectCoverageFrom: [
+    // "src/**/*.ts",
+    // "src/components/*.ts",
+    "!**/node_modules/**",
+    "/*.ts"
+  ],
   coverageThreshold: {
     global: {
       lines: 80,
