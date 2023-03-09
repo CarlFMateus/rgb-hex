9b90c992d07fa373277054313346ea0207988b6e
Carl Mateus <carl.italentt@gmail.com>
2023-03-09 10:55:42 -0500

    Probar con yarn

--- exec.js
+++ exec.js
@@ -1,6 +1,6 @@
 const child_process = require("child_process");
 
-child_process.exec("jest --config ./jest.config.js __tests__/index.test.ts --ci --json --coverage --testLocationInResults --outputFile=report.json", (error, stdout, stderr) => {
+child_process.exec("yarn jest --config ./jest.config.js __tests__/index.test.ts --ci --json --coverage --testLocationInResults --outputFile=report.json", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
