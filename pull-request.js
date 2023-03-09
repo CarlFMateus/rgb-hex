11c07c95d17a4743132bb28d2be3dcf39c87964b
Carl Mateus <carl.italentt@gmail.com>
2023-03-09 12:15:32 -0500

    Probar file externo

---
+++ pull-request.js
@@ -0,0 +1,30 @@
+const util = require("node:util");
+const exec = util.promisify(require("node:child_process").exec);
+
+module.exports = async ({ github, context, core }) => {
+  const number = await context.payload.pull_request.number;
+  const files = await github.rest.pulls.listFiles({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: number,
+  });
+
+  const regex = /.*(spec|test).(ts|js|tsx)$/;
+  const isShow = files.data.some((file) => regex.test(file.filename));
+  if (!isShow) {
+    core.setFailed("Debe contener al menos una prueba");
+  }
+
+  const cadena = files.data.reduce((acc, element) => {
+    if (regex.test(element.filename)) {
+      acc.push(element.filename);
+    }
+    return acc;
+  }, []);
+
+  const { stdout, stderr } = await exec(
+    `yarn jest ${cadena} --ci --json --coverage --testLocationInResults --outputFile=report.json`
+  );
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+};
