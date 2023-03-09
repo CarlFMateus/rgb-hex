33db011d4af4934e5a02358a37040572c8744c4f
Carl Mateus <carl.italentt@gmail.com>
2023-03-09 12:34:48 -0500

    Change logic

--- pull-request.js
+++ pull-request.js
@@ -1,7 +1,7 @@
 const util = require("node:util");
 const exec = util.promisify(require("node:child_process").exec);
 
-module.exports = async ({ github, context, core }) => {
+const validateTest = async ({ github, context, core }) => {
   const number = await context.payload.pull_request.number;
   const files = await github.rest.pulls.listFiles({
     owner: context.repo.owner,
@@ -14,7 +14,17 @@ module.exports = async ({ github, context, core }) => {
   if (!isShow) {
     core.setFailed("Debe contener al menos una prueba");
   }
+}
 
+const runTest = async ({ github, context, core }) => {
+  const number = await context.payload.pull_request.number;
+  const files = await github.rest.pulls.listFiles({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: number,
+  });
+
+  const regex = /.*(spec|test).(ts|js|tsx)$/;
   const cadena = files.data.reduce((acc, element) => {
     if (regex.test(element.filename)) {
       acc.push(element.filename);
@@ -31,5 +41,46 @@ module.exports = async ({ github, context, core }) => {
     console.error('stderr:', stderr);
   } catch (error) {
     console.error(error.message)
+    core.setFailed(error.message)
   }
-};
+}
+
+module.exports = {
+  validateTest,
+  runTest
+}
+
+
+// module.exports = async ({ github, context, core }) => {
+//   const number = await context.payload.pull_request.number;
+//   const files = await github.rest.pulls.listFiles({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     pull_number: number,
+//   });
+
+//   const regex = /.*(spec|test).(ts|js|tsx)$/;
+//   const isShow = files.data.some((file) => regex.test(file.filename));
+//   if (!isShow) {
+//     core.setFailed("Debe contener al menos una prueba");
+//   }
+
+//   const cadena = files.data.reduce((acc, element) => {
+//     if (regex.test(element.filename)) {
+//       acc.push(element.filename);
+//     }
+//     return acc;
+//   }, []);
+
+//   try {
+//     const { stdout, stderr } = await exec(
+//       `yarn jest ${cadena} --config ./jest.config.js --ci --json --coverage --testLocationInResults --outputFile=report.json`
+//     );
+  
+//     console.log('stdout:', stdout);
+//     console.error('stderr:', stderr);
+//   } catch (error) {
+//     console.error(error.message)
+//     core.setFailed(error.message)
+//   }
+// };
