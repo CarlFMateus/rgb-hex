d2d77ab87f07d336fe95d7a51f634163e045cca7
Carl Mateus <carl.italentt@gmail.com>
2023-03-09 14:36:05 -0500

    Continuar flujo

--- pull-request.js
+++ pull-request.js
@@ -10,11 +10,16 @@ const validateTest = async ({ github, context, core }) => {
   });
 
   const regex = /.*(spec|test).(ts|js|tsx)$/;
-  const isShow = files.data.some((file) => regex.test(file.filename));
+  const isShow = files.data.some(
+    (file) => {
+      console.log(file)
+      return regex.test(file.filename) && file.status !== "removed"
+    }
+  );
   if (!isShow) {
     core.setFailed("Debe contener al menos una prueba");
   }
-}
+};
 
 const runTest = async ({ github, context, core }) => {
   const number = await context.payload.pull_request.number;
@@ -25,62 +30,28 @@ const runTest = async ({ github, context, core }) => {
   });
 
   const regex = /.*(spec|test).(ts|js|tsx)$/;
-  const cadena = files.data.reduce((acc, element) => {
-    if (regex.test(element.filename)) {
-      acc.push(element.filename);
-    }
+  const cadena = files.data.reduce((acc, file) => {
+    if (regex.test(file.filename) && file.status !== "removed")
+      acc.push(file.filename);
     return acc;
   }, []);
 
   try {
     const { stdout, stderr } = await exec(
-      `yarn jest ${cadena.join(' ')} --config ./jest.config.js --ci --json --coverage --testLocationInResults --outputFile=report.json`
+      `yarn jest ${cadena.join(
+        " "
+      )} --config ./jest.config.js --ci --json --coverage --testLocationInResults --outputFile=report.json`
     );
-  
-    console.log('stdout:', stdout);
-    console.error('stderr:', stderr);
+
+    console.log("stdout:", stdout);
+    console.error("stderr:", stderr);
   } catch (error) {
-    console.error(error.message)
-    core.setFailed(error.message)
+    console.error(error.message);
+    core.setFailed(error.message);
   }
-}
+};
 
 module.exports = {
   validateTest,
-  runTest
-}
-
-
-// module.exports = async ({ github, context, core }) => {
-//   const number = await context.payload.pull_request.number;
-//   const files = await github.rest.pulls.listFiles({
-//     owner: context.repo.owner,
-//     repo: context.repo.repo,
-//     pull_number: number,
-//   });
-
-//   const regex = /.*(spec|test).(ts|js|tsx)$/;
-//   const isShow = files.data.some((file) => regex.test(file.filename));
-//   if (!isShow) {
-//     core.setFailed("Debe contener al menos una prueba");
-//   }
-
-//   const cadena = files.data.reduce((acc, element) => {
-//     if (regex.test(element.filename)) {
-//       acc.push(element.filename);
-//     }
-//     return acc;
-//   }, []);
-
-//   try {
-//     const { stdout, stderr } = await exec(
-//       `yarn jest ${cadena} --config ./jest.config.js --ci --json --coverage --testLocationInResults --outputFile=report.json`
-//     );
-  
-//     console.log('stdout:', stdout);
-//     console.error('stderr:', stderr);
-//   } catch (error) {
-//     console.error(error.message)
-//     core.setFailed(error.message)
-//   }
-// };
+  runTest,
+};
